8901a79a97a892149508eda01799bc6bf3e4f42e
Jason Lee <A01190466@ad.bcit.ca>
2019-11-18 11:12:23 -0800

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,18 @@ btn.addEventListener('mousedown', (e) => { e.preventDefault();});
 function closeMenu(){
     document.getElementById("navbar").style.height = "0%";
 }
+
+// Scroll to Top
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        
+        $('#return-to-top').fadeIn(200);    
+    } else {
+        $('#return-to-top').fadeOut(200);  
+    }
+});
+$('#return-to-top').click(function() {      
+    $('body,html').animate({
+        scrollTop : 0                       
+    }, 500);
+});
